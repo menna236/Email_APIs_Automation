fb0286749f040e520cd262f833ab3637f71413e4
menna236 <menamahmoud144@gmail.com>
2024-12-21 22:32:41 +0200

    Config updated

--- src/main/java/data/TestData.java
+++ src/main/java/data/TestData.java
@@ -3,6 +3,6 @@ package data;
 public class TestData {
     public static final String disposableEmail = "vebagaj399@iteradev.com";
     public static final String validEmail = "m@gmail.com";
-    public static final String validToken = "92928b756e623357b3bd80e8dc90deae115e1ede921a954bcd87840bd98e946536d8f461bc6815fe6e9e28d943ad21e5";
+    public static final String validToken = "" ; // need to add your token
     public static final String invalidToken = "123";
 }
